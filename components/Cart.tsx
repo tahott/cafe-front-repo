d435e0cca2afc878aebec213dc9d8663bddfc346
tahott <tahott@gmail.com>
2022-10-26 22:51:12 +0900

    feat: Show cart list

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -5,7 +5,25 @@ export function Cart() {
 
   return (
     <div class="container absolute bottom-0 w-full bg-green-200 p-2">
-      <div>{`₩: ${cart.reduce((total, menu) => total + menu.price, 0)}`}</div>
+      <div>
+        {
+          [...cart.reduce((list, curr) => {
+            if (!list.has(curr.name)) {
+              list.set(curr.name, 0)
+            }
+
+            list.set(curr.name, list.get(curr.name) + 1);
+
+            return list;
+          }, new Map())].map(([name, amount]) => (
+            <div class="flex justify-between">
+              <div>{name}</div>
+              <div>{amount}</div>
+            </div>
+          ))
+        }
+      </div>
+      <div class="text-right">{`₩: ${cart.reduce((total, menu) => total + menu.price, 0)}`}</div>
     </div>
   )
 }
